0437ed6449aaa866c2c264065592e4a8092f087e
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-10 18:45:31 +0000

    created an arry with 4 objects, each having a name + src from the available streams

--- src/App.js
+++ src/App.js
@@ -5,6 +5,29 @@ import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
 import "./App.css";
 
+const streams = [
+  {
+    name: "Big Buck Bunny",
+    src:
+      "https://multiplatform-f.akamaihd.net/i/multi/april11/sintel/sintel-hd_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8",
+  },
+  {
+    name: "HD World",
+    src:
+      "https://multiplatform-f.akamaihd.net/i/multi/april11/hdworld/hdworld_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8",
+  },
+  {
+    name: "Sintel",
+    src:
+      "https://multiplatform-f.akamaihd.net/i/multi/april11/sintel/sintel-hd_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8",
+  },
+  {
+    name: "Parkour",
+    src:
+      "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+  },
+];
+
 class App extends Component {
   constructor(props) {
     super(props);
