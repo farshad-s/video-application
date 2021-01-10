0fad6e78bc3b68a54f684c071bc9ab09d0d692fd
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-10 18:51:28 +0000

    removed sources

--- src/options.js
+++ src/options.js
@@ -1,12 +1,6 @@
 const videoJsOptions = {
   autoplay: false,
   controls: true,
-  sources: [
-    {
-      src:
-        "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
-    },
-  ],
 };
 
 export default videoJsOptions;
