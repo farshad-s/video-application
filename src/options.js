1b4a2c32a38d059f997a24831ac6a75f54b6fb61
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-10 18:05:04 +0000

    created folder for video options

---
+++ src/options.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+import Videojs from "./video.js";
+
+const videoJsOptions = {
+  autoplay: false,
+  controls: true,
+  sources: [
+    {
+      src: "",
+      type: "",
+    },
+  ],
+};
+
+export default videoJsOptions;
