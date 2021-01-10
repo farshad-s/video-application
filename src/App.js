878eaf2fcf9ac9346c600e46a5504e3f8f21286c
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-10 18:37:19 +0000

    passed classname to container + buttons

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import Videojs from "./video.js";
 import videoJsOptions from "./options";
 import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
+import "./App.css";
 
 function App() {
   return (
-    <div className="d-flex flex-column align-items-center">
+    <div className="d-flex flex-column align-items-center container">
       <Videojs {...videoJsOptions} />
-      <Dropdown>
+      <Dropdown className="btn">
         <Dropdown.Toggle variant="success" id="dropdown-basic">
           Video Streams
         </Dropdown.Toggle>
 
         <Dropdown.Menu className="d-flex flex-column align-items-center">
-          <Button>Video 1</Button>
-          <Button>Video 2</Button>
-          <Button>Video 3</Button>
-          <Button>Video 4</Button>
+          <Button className="btn">Video 1</Button>
+          <Button className="btn">Video 2</Button>
+          <Button className="btn">Video 3</Button>
+          <Button className="btn">Video 4</Button>
         </Dropdown.Menu>
       </Dropdown>
     </div>
