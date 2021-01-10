1be55be0f4aa404958fed9c73db8d382dc557f39
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-10 19:36:49 +0000

    changed videojs player to Player from video-react + changed m3u8 files to mp4 files

--- src/components/App.js
+++ src/components/App.js
@@ -4,27 +4,27 @@ import videoJsOptions from "./options";
 import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
 import "../App.css";
+import { Player } from "video-react";
 
 const streams = [
   {
     name: "Big Buck Bunny",
     src:
-      "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
   },
   {
-    name: "HD World",
+    name: "Blender Open Movie",
     src:
-      "https://multiplatform-f.akamaihd.net/i/multi/april11/hdworld/hdworld_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
   },
   {
-    name: "Sintel",
+    name: "Introducing ChromeCast",
     src:
-      "https://multiplatform-f.akamaihd.net/i/multi/april11/sintel/sintel-hd_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
   },
   {
-    name: "Parkour",
-    src:
-      "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+    name: "Lego Video",
+    src: "http://techslides.com/demos/sample-videos/small.webm",
   },
 ];
 
@@ -39,7 +39,7 @@ class App extends Component {
   render() {
     return (
       <div className="d-flex flex-column align-items-center container">
-        <Videojs {...videoJsOptions} sources={this.state.currentStream.src} />
+        <Player src={this.state.currentStream.src} />
         <Dropdown className="btn">
           <Dropdown.Toggle variant="success" id="dropdown-basic">
             Video Streams
