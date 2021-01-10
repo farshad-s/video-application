c67f0301e4042f7462a0ac30d8f40c4dad4484b5
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-10 18:42:58 +0000

    imported Component and changed function to class component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Videojs from "./video.js";
 import videoJsOptions from "./options";
 import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="d-flex flex-column align-items-center container">
-      <Videojs {...videoJsOptions} />
-      <Dropdown className="btn">
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Video Streams
-        </Dropdown.Toggle>
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="d-flex flex-column align-items-center container">
+        <Videojs {...videoJsOptions} />
+        <Dropdown className="btn">
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Video Streams
+          </Dropdown.Toggle>
 
-        <Dropdown.Menu className="d-flex flex-column align-items-center">
-          <Button className="btn">Video 1</Button>
-          <Button className="btn">Video 2</Button>
-          <Button className="btn">Video 3</Button>
-          <Button className="btn">Video 4</Button>
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
-  );
+          <Dropdown.Menu className="d-flex flex-column align-items-center">
+            <Button className="btn">Video 1</Button>
+            <Button className="btn">Video 2</Button>
+            <Button className="btn">Video 3</Button>
+            <Button className="btn">Video 4</Button>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    );
+  }
 }
 
 export default App;
