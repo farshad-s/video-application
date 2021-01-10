d04dbcb0313334e512b74b7a8189b27ac93f078f
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-10 18:20:54 +0000

    imported dropdown + button & placed into the return statement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Videojs from "./video.js";
 import videoJsOptions from "./options";
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="d-flex justify-content-center">
       <Videojs {...videoJsOptions} />
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Button>Video 1</Button>
+          <Button>Video 2</Button>
+          <Button>Video 3</Button>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 }
