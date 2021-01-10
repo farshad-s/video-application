5a5046424168067b01f4016351a7c7b91cc93f12
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-10 18:11:40 +0000

    imported render

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { render } from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
